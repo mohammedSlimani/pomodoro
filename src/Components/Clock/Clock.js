0785da8bd7d26b6a85d7365bbe1f5ccc470fe131
Mohammed slimani <slimani.med.21@gmail.com>
2019-03-26 21:12:54 +0000

    Added The main components

---
+++ src/Components/Clock/Clock.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react'
+
+//Display the Countdown and stuff!
+export class Clock extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            maxTime:props.givenTime,
+            timeLeft:props.givenTime*60,
+            intervalID:''
+        }
+        this.startCountDown =  this.startCountDown.bind(this);
+        this.timer = this.timer.bind(this);
+        this.clockSetter = this.clockSetter.bind(this);
+        this.reset = this.reset.bind(this);
+        this.buzzer = this.buzzer.bind(this);
+    }
+    componentWillReceiveProps(nextProps){
+        if(this.props.isPaused){
+        if(this.state.maxTime !== nextProps.givenTime*60){
+            this.setState({
+                maxTime:nextProps.givenTime*60,
+                timeLeft:nextProps.givenTime*60
+            });
+        }
+    }
+    }
+    startCountDown(){
+        console.log("in start countDown: TimeLeft:"+this.state.timeLeft);
+        this.setState({
+            intervalID:setInterval(this.timer,100)
+        });   
+    }
+    timer(){
+        console.log("Calling the Timer");
+        let count = this.state.timeLeft - 1;
+        if(count>=0){
+            if(count==0) this.buzzer();
+            this.setState({timeLeft:count});
+        }else{
+            this.props.switchPhase(); //send a signal to do the next step 
+            this.setState({
+                timeLeft:this.props.givenTime*60
+            })
+        }
+    }
+    clockSetter(){
+        console.log("calling colckSetter isPaused:");
+        this.props.pause();
+        if(this.props.isPaused){
+            console.log("in the clock Setter calling startcountdown next");
+            this.startCountDown();
+        }else{
+            clearInterval(this.state.intervalID);
+        }
+    }
+    reset(){
+        if(!this.props.isPaused) this.clockSetter();
+        this.props.reset();
+        this.setState({
+            maxTime:25*60,
+            timeLeft:25*60
+        });
+        const sound = document.getElementById('beep');
+        sound.pause();
+        sound.currentTime=0;
+    }
+    buzzer(){
+        const sound = document.getElementById('beep');
+        sound.play();
+    }
+  render() {
+      let min=Math.floor(this.state.timeLeft/60).toString();
+      let sec=(this.state.timeLeft%60).toString(); 
+    return (
+      <div>
+          <div id='timer-label'>{this.props.isSession?'session':'break'}</div>
+          <div id='time-left'>{min<10? '0'+min:min} : {sec<10? '0'+sec:sec}</div>
+          <br/>
+          <audio id='beep' src='https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3'></audio>
+          <button id='start_stop' onClick={this.clockSetter}>{this.props.isPaused? 'start':'pause'}</button>
+          <button id='reset' onClick ={this.reset}>reset</button>
+      </div>
+    )
+  }
+}
+
+export default Clock
